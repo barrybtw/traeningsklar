e5746410ed299a7317035b6de9fb3a17916e7098
Nicolai Christensen <nicolaibernhardchristensen@gmail.com>
2025-08-17 21:44:41 +0200

    Så burde auth virke

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,17 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma-client";
+import { admin } from "better-auth/plugins";
 
 export const auth = betterAuth({
   socialProviders: {
-    google: { clientId: "", clientSecret: "" },
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_SECRET_KEY!,
+    },
   },
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  plugins: [admin()],
 });
